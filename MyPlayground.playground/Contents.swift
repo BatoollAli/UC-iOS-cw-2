b5d751b22d607afc618c0f0b0cad340a7e59f9fa
Batool Ali <batoolaliismail@gmail.com>
2022-06-25 19:07:47 +0300

    finish cw1.1
    finish

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,14 @@
+import UIKit
+import Foundation
+
+var greeting = "Hello, playground"
+
+var favouriteNumber = 3
+var name = "Batool "
+var hobby = "swimming"
+let BirthOfDate = 2003
+    
+print("My name is \(name) . My favouriteNumber is \(favouriteNumber). My hobby is \(hobby)")
+
+print("اسمي \(name ). رقمي المفضل \(favouriteNumber). هوايتي \(hobby). سنه الميلاد \(BirthOfDate)")
+
